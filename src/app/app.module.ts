3c356be5ac598940e43d50ab152dde38593183bd
ashaaut <ashaautade1998@gmail.com>
2021-08-19 10:13:28 +0530

    added two way binding  with ndModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';//load es6 module
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ //register component/directives and pipes
     AppComponent
   ],
   imports: [ //helper modules or utility or dependancy module
-    BrowserModule
+    BrowserModule,FormsModule
   ],
   providers: [], //register and launch services
   bootstrap: [AppComponent] //load components
